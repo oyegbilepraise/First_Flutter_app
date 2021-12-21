33e97b52f333387d2dedf78b31660cadd845e612
Praise <oluwadhammieh@gmail.com>
2021-12-21 11:31:10 +0100

    Added Question.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerChosen() {
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(
+      'Answer Selected!',
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your fav color',
+      'What\'s your fav animal',
+    ];
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('My First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerChosen,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerChosen,
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerChosen,
+            ),
+          ],
+        ),
       ),
-      body: Text('This is my default text!'),
-    ));
+    );
   }
 }
